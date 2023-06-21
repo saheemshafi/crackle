705e8753f2a4b4e58043ecdbbb1147e120389981
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2023-06-21 20:52:29 +0530

    Fix

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { downloadImage } from "@/lib/helpers/download-image";
 import { toastOptions } from "@/lib/utlities/toast";
 import * as Backdrop from "@/types/backdrop";
 import Image from "next/image";
 import { FC, HTMLAttributes, useState } from "react";
 import toast from "react-hot-toast";
-import { BiErrorAlt } from "react-icons/bi";
+import { BiCheck, BiErrorAlt } from "react-icons/bi";
 import { HiOutlineDownload } from "react-icons/hi";
 import { twMerge } from "tailwind-merge";
+import * as uuid from "uuid"
 
 interface ImageCardProps extends HTMLAttributes<HTMLDivElement> {
   image: Backdrop.Image;
@@ -17,6 +17,56 @@ interface ImageCardProps extends HTMLAttributes<HTMLDivElement> {
 const ImageCard: FC<ImageCardProps> = ({ image, className }) => {
   const [downloading, setDownloading] = useState(false);
 
+ async function downloadImage(
+    path: string,
+    width: number,
+    height: number
+  ) {
+    if (typeof document == "undefined") return;
+    const request = fetch(
+      `/api/og?path=${
+        path.slice(1).split(".")[0]
+      }&width=${width}&height=${height}&ext=${path.split(".")[1]}`
+    ).then((res) => res.blob());
+  
+    const blob = await toast.promise(
+      request,
+      {
+        error: (
+          <div className="flex items-center gap-2">
+            <BiErrorAlt className="mr-3 text-brand" size={20} /> Oops! Something
+            went wrong.
+          </div>
+        ),
+        loading: (
+          <div className="flex flex-col">
+            <div className="flex w-full items-center gap-2">
+              <div className="aspect-square h-4 w-4 animate-spin rounded-full border-2 border-r-transparent "></div>
+              Starting Download...
+            </div>
+            <p className="mt-1 block text-sm text-gray-light">
+              Large images might take some time to download
+            </p>
+          </div>
+        ),
+        success: (
+          <div className="flex items-center gap-2">
+            <BiCheck size={20} className="animate-in mr-3 text-brand" /> Download
+            Started!
+          </div>
+        ),
+      },
+      { ...toastOptions, icon: null }
+    );
+  
+    const url = URL.createObjectURL(blob);
+    const anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.download = `crackle-${uuid.v1()}.${path.split(".")[1]}`;
+    anchor.click()
+    anchor.remove()
+  }
+  
   return (
     <figure className={"relative rounded-lg bg-gray-dark p-2"}>
       <div className="group relative rounded-md border border-gray-md/30">
